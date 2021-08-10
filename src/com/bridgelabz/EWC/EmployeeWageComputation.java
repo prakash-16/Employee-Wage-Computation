8ca5e17c84a9bc12aaf221fcd88762876c6e7468
Prakash-16 <goudprakash58@gmail.com>
2021-08-10 18:44:38 +0530

    [Prakash] - Added Employee Attendance Feature

--- src/com/bridgelabz/EWC/EmployeeWageComputation.java
+++ src/com/bridgelabz/EWC/EmployeeWageComputation.java
@@ -5,6 +5,15 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		System.out.println(" Welcome to Employee Computation Program ");
+		int emp_present = 1, emp_result_attendance;
+		double check_emp_attendance = Math.floor(Math.random()*10)%2;
+		emp_result_attendance = (int)check_emp_attendance;
+		if(emp_present == (emp_result_attendance)){
+			System.out.println(" Employee is Present ");
+		}	
+		else {
+			System.out.println(" Employee is Absent ");
+		}
 	}
 
 }
